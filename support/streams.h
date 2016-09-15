231a7b992fe7d6f30fb715de8062df745e3557b4
toastedcornflakes <toastedcornflakes@gmail.com>
2016-09-15 14:06:00 +0200

    Include guards for support/*

--- support/streams.h
+++ support/streams.h
@@ -1,1 +1,6 @@
+#ifndef STREAMS_H
+#define STREAMS_H
+
 void streams_setup();
+
+#endif
--- support/uart.h
+++ support/uart.h
@@ -1,6 +1,11 @@
+#ifndef UART_H
+#define UART_H
+
 #include <stdint.h>
 
 void uart_setup();
 
 void uart_transmit(uint8_t data);
 uint8_t uart_receive();
+
+#endif
