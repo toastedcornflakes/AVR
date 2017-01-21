1fc1b6fda39cac71be15c910791c692c4efc600f
toastedcornflakes <toastedcornflakes@gmail.com>
2017-01-21 12:14:16 +0100

    Only switch LED during button fall

--- main.c
+++ main.c
@@ -8,18 +8,27 @@
 #include <avr/wdt.h>
 
 #include <util/delay.h>
+#include <util/atomic.h>
 
 #include "support/uart.h"
 
-volatile uint8_t button = 0;
-
-ISR(PCINT0_vect) {
-	button = 1;
-}
+volatile uint8_t can_switch = 1;
 
 static void switch_leds() {
 	PORTC ^= _BV(PC3);
 	PORTC ^= _BV(PC5);
+}
+
+ISR(PCINT0_vect) {
+	// check that we have a falling edge (i.e. just pressed the button)
+
+	if (!(PINB & _BV(PC0))) {
+
+		if (can_switch) {
+			switch_leds();
+		}
+		can_switch = 0;
+	}
 }
 
 static void setup_button_ISR() {
@@ -28,9 +37,12 @@ static void setup_button_ISR() {
 }
 
 static void setup_pins() {
+	// set PB0 as input
+	DDRB &= ~(_BV(DDB0));
+
 	// set PC3 and PC5 as output 
 	DDRC |= (_BV(DDC3) | _BV(DDC5));
-	
+
 	// set PC5 as high at the start
 	PORTC |= _BV(PC5);
 }
@@ -50,17 +62,21 @@ int main()
 	setup();
 	puts("Setup done");
 
+	uint8_t can_switch_cnt = 0;
 	while(1)
 	{
-		if(button == 1) {
-			puts("Button!");
-			switch_leds();
-			// Clear only after a delay
-			_delay_ms(200);
-			button = 0;
-		}
+		_delay_ms(50);
 
-		_delay_ms(100);
+		ATOMIC_BLOCK(ATOMIC_FORCEON)
+		{
+			if(!can_switch) {
+				can_switch_cnt++;
+				if( can_switch_cnt > 2) {
+					can_switch = 1;
+					can_switch_cnt = 0;
+				}
+			}
+		}
 
 		wdt_reset();
 	}
